887b324872961d7214f0aac2b1933ab041c6ac21
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-03-12 01:32:52 +0300

    f():

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,13 +22,13 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
-        <div className='hero__image-container'>
-          <div className='hero__image'>
-            <Image src='/hero.png' alt='hero' fill className='object-contain' />
-            <div className='hero__image-overlay'></div>
-          </div>
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image src='/hero.png' alt='hero' fill className='object-contain' />
         </div>
+        <div className='hero__image-overlay' />
       </div>
+    </div>
   );
 };
 
