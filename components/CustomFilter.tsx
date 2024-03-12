2a26e13fe9792362a40b2fdb32bf88745f45b092
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-03-12 13:51:12 +0300

    feat(Searchbar,Filter): create searchbar and filter

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,8 @@
+import { CustomFilterProps } from "@/types";
+import React from "react";
+
+const CustomFilter = (title:CustomFilterProps) => {
+  return <div>CustomFilter</div>;
+};
+
+export default CustomFilter;
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,7 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
 }
+
+export interface CustomFilterProps {
+  title: string;
+}
