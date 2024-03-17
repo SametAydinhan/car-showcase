cefb3d9c4334cdf9ccb5cf02b22317d177f84eb4
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-03-17 15:41:48 +0300

    fix(customfilter): searching scroll bug fix

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,21 +5,27 @@ import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
+import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
 
-  const handleUpdateParams = (type: string, value: string) => {
-    const newPathname = "";
-    
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathname = updateSearchParams(title, e.value.toLowerCase());
 
-    router.push(newPathname);
+    router.push(newPathname,{scroll:false});
   };
 
   return (
     <div className='w-fit'>
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
+      >
         <div className='relative w-fit z-10'>
           <Listbox.Button className='custom-filter__btn'>
             <span className='block truncate'>{selected.title}</span>
