4d7844c7f8b4916c3bdc8509114ca6ab42bcf55a
Samet Aydinhan <samet.aydinhan879@gmail.com>
2024-03-17 14:13:19 +0300

    feat(cumstomfilter): create customfilter

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 
 import Image from "next/image";
@@ -27,8 +28,8 @@ export default async function Home({searchParams}) {
           <SearchBar />
 
           <div className='home__filter-container'>
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
           </div>
         </div>
 
